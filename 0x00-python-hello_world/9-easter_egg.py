743adcb735e80d60415168a95ce58496e371df0b
MikeNdlovu <mickeymusa03@gmail.com>
2023-11-27 22:50:56 -0800

    import

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -1,24 +1,2 @@
 #!/usr/bin/python3
-ibt = " is better than "
-iti = "If the implementation is hard to explain, it"
-print(f"""\
-The Zen of Python, by Tim Peters
-
-Beautiful{ibt}ugly.
-Explicit{ibt}implicit.
-Simple{ibt}complex.
-Flat{ibt}nested.
-Sparse{ibt}dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-{iti}'s a bad idea.
-{iti} may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!" """)
+import test
