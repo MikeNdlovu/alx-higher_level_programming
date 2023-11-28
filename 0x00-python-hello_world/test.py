e82c42d90df05a55c7d3a41ba903a6e62a06c4c2
MikeNdlovu <mickeymusa03@gmail.com>
2023-11-27 22:54:12 -0800

    import

--- 0x00-python-hello_world/test.py
+++ 0x00-python-hello_world/test.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-ibt = " is better than "
-iti = "If the implementation is hard to explain, it"
-
 print(f"""\
 The Zen of Python, by Tim Peters
 
-Beautiful{ibt}ugly.
-Explicit{ibt}impliciit.
-Simple{ibt}complex.
-Flat{ibt}nested.
-Sparse{ibt}dense.
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
 Readability counts.
 Special cases aren't special enough to break the rules.
 Although practicality beats purity.
@@ -20,6 +18,6 @@ There should be one-- and preferably only one --obvious way to do it.
 Although that way may not be obvious at first unless you're Dutch.
 Now is better than never.
 Although never is often better than *right* now.
-{iti}'s a bad idea.
-{iti} may be a good idea.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those! """)
