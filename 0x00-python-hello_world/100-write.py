049b2bf65d93446200872a5682df50755e9d64bc
MikeNdlovu <mickeymusa03@gmail.com>
2023-11-29 08:33:53 -0800

    eprint: printing to stderr with exit status 1

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,9 +2,8 @@
 import sys
 
 
-def eprint(*args, **kwargs):
-    print(*args, file=sys.stderr, **kwargs)
+def write():
+    sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
     exit(1)
 
-
-eprint("and that piece of art is useful - Dora Korpar, 2015-10-19")
+write()
