9c17b78a56b54d8145c3726a36cd19d08f27a8bb
MikeNdlovu <mickeymusa03@gmail.com>
2023-12-04 09:48:25 -0800

    my own pop function

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -8,6 +8,8 @@ def delete_at(my_list=[], idx=0):
         while n < len(my_list):
             if n == idx:
                 n = idx + 1
+            if n == len(my_list):
+                break
             new_list.append(my_list[n])
             n += 1
         my_list = new_list
