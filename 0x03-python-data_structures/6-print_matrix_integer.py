0c1aa312861449d96ff2aa32aca545ae95c8ff15
MikeNdlovu <mickeymusa03@gmail.com>
2023-12-04 04:36:01 -0800

    printing a matrix of ints

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in matrix:
-        remove = str.maketrans({"[": "", "]": "", ",": " "})
-        matrix = str(i).translate(remove)
-        print(matrix)
+    n = 0
+    while n < 3:
+        if matrix[n] == []:
+            print()
+            break
+        else:
+            p = 0
+            while p < 3:
+                print("{}".format(int(matrix[n][p])), end=" ")
+                p += 1
+        print()
+        n += 1
+#   for i in matrix:
+ #       remove = str.maketrans({"[": "", "]": "", ",": " "})
+  #      matrix = str(i).translate(remove)
+   #     print("{}".format(int(matrix))) 
