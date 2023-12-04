b2d65073315dabce6ce7820ad9e1a09b51f163f8
MikeNdlovu <mickeymusa03@gmail.com>
2023-12-04 02:31:57 -0800

    prints a matrix of integers

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    remove = str.maketrans({"c":"", "C":""})
+    remove = str.maketrans({"c": "", "C": ""})
     my_string = my_string.translate(remove)
     return my_string
