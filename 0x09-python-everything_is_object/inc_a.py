0450305789984e16be173b1e5ec78e76da9dfd29
MikeNdlovu <mickeymusa03@gmail.com>
2024-01-05 06:13:01 -0800

    returns a string n times the numebr of iteration

---
+++ 0x09-python-everything_is_object/inc_a.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" increments a variable by 1 """
+
+
+def increment(n):
+    """ n is the int being increased """
+    n = n + [111]
+    print(n)
+
+a = [1, 2, 3, 4]
+increment(a)
+print(a)
