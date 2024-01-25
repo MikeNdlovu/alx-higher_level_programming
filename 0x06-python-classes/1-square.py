ae18b7745ae003b8bd7a202d466dbbf3a748e934
MikeNdlovu <mickeymusa03@gmail.com>
2024-01-25 03:18:25 -0800

    a class Square with with private attr size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" defines a class with private attr size """
+
+
+class Square:
+    """ defines private attr size """
+    def __init__(self, size):
+        self._Square__size = size
